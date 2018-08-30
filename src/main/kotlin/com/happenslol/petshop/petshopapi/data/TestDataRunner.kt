1ec457c480f009d662c47efc70ce54559ecd0de0
Hilmar Wiegand <me@hwgnd.de>
2018-08-30 18:05:25 +0200

    :sparkles: Add test data service and runner

--- src/main/kotlin/com/happenslol/petshop/petshopapi/data/TestDataRunner.kt
+++ src/main/kotlin/com/happenslol/petshop/petshopapi/data/TestDataRunner.kt
@@ -1,2 +1,20 @@
 package com.happenslol.petshop.petshopapi.data
 
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class TestDataRunner(
+    val testDataService: TestDataService
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        if (args.getOrNull(0) != "test-data")
+            return
+
+        testDataService.deleteAll()
+        println("emptied database")
+
+        testDataService.loadTestData()
+        println("loaded test data")
+    }
+}
--- src/main/kotlin/com/happenslol/petshop/petshopapi/data/TestDataService.kt
+++ src/main/kotlin/com/happenslol/petshop/petshopapi/data/TestDataService.kt
@@ -1,2 +1,38 @@
 package com.happenslol.petshop.petshopapi.data
 
+import com.happenslol.petshop.petshopapi.animals.Animal
+import com.happenslol.petshop.petshopapi.animals.AnimalRepository
+import com.happenslol.petshop.petshopapi.animals.AnimalSpeciesRepository
+import com.happenslol.petshop.petshopapi.animals.Species
+import com.happenslol.petshop.petshopapi.employees.Employee
+import com.happenslol.petshop.petshopapi.employees.EmployeeRepository
+import com.happenslol.petshop.petshopapi.food.FoodStock
+import com.happenslol.petshop.petshopapi.food.FoodStockRepository
+import org.springframework.stereotype.Service
+
+@Service
+class TestDataService(
+    val foodStockRepository: FoodStockRepository,
+    val animalRepository: AnimalRepository,
+    val animalSpeciesRepository: AnimalSpeciesRepository,
+    val employeeRepository: EmployeeRepository
+) {
+    fun deleteAll() {
+        foodStockRepository.deleteAll()
+        animalRepository.deleteAll()
+        animalSpeciesRepository.deleteAll()
+        employeeRepository.deleteAll()
+    }
+
+    fun loadTestData() {
+        foodStockRepository.saveAll(foodStockData())
+        animalRepository.saveAll(animalData())
+        animalSpeciesRepository.saveAll(animalSpeciesData())
+        employeeRepository.saveAll(employeeData())
+    }
+
+    fun foodStockData(): List<FoodStock> = listOf()
+    fun animalData(): List<Animal> = listOf()
+    fun animalSpeciesData(): List<Species> = listOf()
+    fun employeeData(): List<Employee> = listOf()
+}
