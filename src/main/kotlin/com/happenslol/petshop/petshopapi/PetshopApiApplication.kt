516a3c46291cce90c94a797c5c1b0ce291a034de
Hilmar Wiegand <me@hwgnd.de>
2018-08-30 16:19:49 +0200

    :tada: Initial commit

---
+++ src/main/kotlin/com/happenslol/petshop/petshopapi/PetshopApiApplication.kt
@@ -0,0 +1,11 @@
+package com.happenslol.petshop.petshopapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetshopApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PetshopApiApplication>(*args)
+}
---
+++ src/main/kotlin/com/happenslol/petshop/petshopapi/food/FoodService.kt
@@ -0,0 +1,2 @@
+package com.happenslol.petshop.petshopapi.food
+
